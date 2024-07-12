a3a81b0a15a7f90f39dff744dceed9d8078e040e
laleti28 <163922071+laleti28@users.noreply.github.com>
2024-07-12 20:29:03 +0200

    git commit -m commit all my cahnges to my local repository

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ function validateSyntax() {
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+        
+    // Regular expression to match 'pet_' and followed by alphanumeric characters      
+  let regex = /^pet_[a-zA-Z0-9]+$/;
+  // Check if input starts with 'pet_' and followed by alphanumeric characters
+  if (regex.test(input)) {
+    result = "Valid Syntax🟢";
+  } else {
+    result = "Invalid Syntax🔴";
+  }
 
-            document.getElementById('result').innerText = result;
+  document.getElementById('result').innerText = result;
+
 }
-
-
